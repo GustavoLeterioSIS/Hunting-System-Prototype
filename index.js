d91d2fe0912e945576d1725df4d26b1a5e4ccedf
Gustavo Leterio <gustavo.leterio@sisconsultoria.com.br>
2022-11-07 10:54:14 -0300

    Open and Close Sidebar/HeaderDropdown logic update

--- index.js
+++ index.js
@@ -1,76 +1,67 @@
-//Sidebar Handling
+//Sidebar - sb
 var sidebar = document.getElementById("sidebar");
-var sidebarButton = sidebar.children[0];
+var sbButton = sidebar.children[0];
+
+//SB Inner Dropdown
+const sbDropdownTrigger = document.getElementById("sbDropdownTrigger");
+const sbDropdownIcon = document.getElementById("dropdownIcon");
+const sbDropdownContent = document.getElementById("sbDropdownContent");
+
+//Header - hd
+var header = document.getElementById("header");
+var hdTrigger = document.getElementById("hdDropdownTrigger");
+const hdDropdownContent = document.getElementById("hdDropdownContent");
+
 const iframe = document.getElementById("iframe");
 
+//Global Event
 window.addEventListener('click', function (e) {
-  if (sidebarButton.contains(e.target)) {
-    sbButtonHandle();
-    sideIcon.classList.toggle("displayed");
-    sidebar.classList.toggle("open");
-    resetSidebarDropdown();
-  }else{
+  //Sidebar Pseudo Event
+  if (sbButton.contains(e.target)) {
+    openSidebar();
+  } else if (!sidebar.contains(e.target)) {
+    closeSidebar();
   }
 
+  //Sidebar Inner Dropdown Pseudo Event
+  if (sbDropdownTrigger.contains(e.target)) {
+    sbDropdownContent.classList.toggle("open");
+    sbDropdownIcon.classList.toggle("open");
+  }
+
+  //Header Dropdown Pseudo Event
+  if (hdTrigger.contains(e.target)) {
+    setTimeout(() => {
+      hdDropdownContent.classList.toggle("displayed");
+    }, 10);
+    } else if (!header.contains(e.target)) {
+      hdDropdownContent.classList.remove("displayed");
+    }
 });
 
-const sbButtonHandle = () => {
-  sidebarButton.classList.toggle('opened');
-  sidebarButton.setAttribute('aria-expanded', sidebarButton.classList.contains('opened'));
+//The window Event can't capture the iframe onClick, the solution to close everything is creating a new Event.
+//For each Iframe's Reload, a new contentWindow Event Must be created, because the iframe forgot it event.
+iframe.addEventListener("load", () => {
+  iframe.contentWindow.document.body.addEventListener("click", () => {
+    hdDropdownContent.classList.remove("displayed");
+    closeSidebar();
+  });
+});
+
+const openSidebar = () => {
+  sbDropdownIcon.classList.toggle("displayed");
+  sbButton.classList.add('opened');
+  sidebar.classList.toggle("open");
+}
+
+const closeSidebar = () => {
+  resetSidebarDropdown();
+  sbDropdownIcon.classList.remove("displayed");
+  sbButton.classList.remove('opened');
+  sidebar.classList.remove("open");
 }
 
 const resetSidebarDropdown = () => {
   sbDropdownContent.classList.remove("open");
-  sideIcon.classList.remove("open");
+  sbDropdownIcon.classList.remove("open");
 }
-
-//Sidebar Intern Dropdown Menu Handling
-const sidebarDropdownTrigger = document.getElementById("sbDropdownTrigger");
-const sideIcon = document.getElementById("dropdownIcon");
-const sbDropdownContent = document.getElementById("sbDropdownContent");
-
-sidebarDropdownTrigger.addEventListener("click", () => {
-  sbDropdownContent.classList.toggle("open");
-  sideIcon.classList.toggle("open");
-});
-
-//Header Dropdown Handling
-var header = document.getElementById("header");
-var headerTrigger = document.getElementById("hdDropdownTrigger");
-const hdDropdownContent = document.getElementById("hdDropdownContent");
-
-headerTrigger.addEventListener("click", () => {
-  setTimeout(() => {
-    hdDropdownContent.classList.add("displayed");
-  }, 10);
-});
-
-//Dropdown Closing System
-
-//For each reload of the iframe I needed to create a new event
-iframe.addEventListener("load", () => {
-  iframe.contentWindow.document.body.addEventListener("click", () => {
-    hdDropdownContent.classList.remove("displayed");
-    if (sidebar.classList[1] == "open") {
-      resetSidebarDropdown();
-      sideIcon.classList.remove("displayed");
-      sbButtonHandle();
-      sidebar.classList.remove("open");
-    }
-  });
-});
-
-//DD sidebar closing
-header.addEventListener("click", () => {
-  if (sidebar.classList[1] == "open") {
-    resetSidebarDropdown();
-    sideIcon.classList.remove("displayed");
-    sbButtonHandle();
-    sidebar.classList.remove("open");
-  }
-});
-
-//DD Header closing
-sidebar.addEventListener("click", () => {
-  hdDropdownContent.classList.remove("displayed");
-});
